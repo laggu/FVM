189b8699a1078052c11f7155f0fba4acff2ab8a7
laggu <kwla103@naver.com>
2018-06-04 16:26:28 +0900

    프로젝트 생성

---
+++ src/Init.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+public class Init extends BaseCommand{
+    @Override
+    public void execute() {
+    }
+
+    public void execute(String str) {
+        File dir = new File(".fvm");
+        dir.mkdirs();
+    }
+}
