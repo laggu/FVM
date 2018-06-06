4c993ad960d80a2063f5944234733f4ce7e62e13
Sangyeon <39687820+Billy-Kimm@users.noreply.github.com>
2018-06-06 12:29:13 +0900

    Add files via upload

---
+++ JDBCUtil.java
@@ -0,0 +1,49 @@
+package util;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class JDBCUtil {										// DB와 CONNECTION 관리 클래스
+	
+	public static Connection getConnection () {
+		
+		Connection con = null;
+		String driver = "";
+		String url = "";
+		String user = "";
+		String pw = "";
+		
+		try {												// Properties, key(string) - value(string)
+			Properties p = new Properties(); 				 
+			p.load(new FileInputStream("C:\\lib\\db_info.txt"));  
+			driver = p.getProperty("driver");
+			url = p.getProperty("url");
+			user = p.getProperty("user");
+			pw = p.getProperty("pw");
+			
+			Class.forName(p.getProperty("driver"));
+			
+			
+			con = DriverManager.getConnection(url, user, pw);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+															// 자원관리 클래스
+	public static void close(Connection con, Statement st, ResultSet rs) {	
+		try {
+			if(rs!=null) rs.close();
+			if(st!=null) st.close();
+			if(con!=null) con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
---
+++ Vo_Remote.java
@@ -0,0 +1,40 @@
+package db;
+
+public class Vo_Remote {
+
+	String Id;
+	String pw;
+
+	public Vo_Remote() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Vo_Remote(String id, String pw) {
+		super();
+		Id = id;
+		this.pw = pw;
+	}
+
+	public String getId() {
+		return Id;
+	}
+
+	public void setId(String id) {
+		Id = id;
+	}
+
+	public String getPw() {
+		return pw;
+	}
+
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+
+	@Override
+	public String toString() {
+		return "Vo_Remote [Id=" + Id + ", pw=" + pw + "]";
+	}
+
+}
