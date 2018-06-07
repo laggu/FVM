6d2a32dadca4f8f987152a146ca51575bfe6b319
eileenkim1208 <eileenkim1208@gmail.com>
2018-06-07 19:48:07 +0900

    ui local update

---
+++ src/UI/local/south/CommitButton.java
@@ -0,0 +1,13 @@
+package ui.local.south;
+
+import java.awt.Font;
+
+import javax.swing.JButton;
+
+public class CommitButton extends JButton {
+	public CommitButton(){
+		this.setText("Commit");
+		this.setFont(new Font("Times New Roman", Font.BOLD, 20));
+	}
+
+}
