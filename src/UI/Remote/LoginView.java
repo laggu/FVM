372cd1bd48ecb2e26f9a34d89c6f4599164301da
xo4813 <tjxogks@naver.com>
2018-06-07 15:24:49 +0900

    remote

---
+++ src/UI/Remote/LoginView.java
@@ -0,0 +1,93 @@
+package UI.Remote;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class LoginView extends JDialog {
+
+	private JPasswordField passText;
+	private JTextField userText;
+	private boolean bLoginCheck;
+
+	public LoginView() {
+		// setting
+		setTitle("login");
+		setSize(280, 150);
+		setResizable(false);
+		setLocation(800, 450);
+
+		// panel
+		JPanel panel = new JPanel();
+		placeLoginPanel(panel);
+
+		// add
+		add(panel);
+
+		// visiible
+		setVisible(true);
+	}
+
+	private void placeLoginPanel(JPanel panel) {
+		panel.setLayout(null);
+		JLabel userLabel = new JLabel("User");
+		userLabel.setBounds(10, 10, 80, 25);
+		panel.add(userLabel);
+
+		JLabel passLabel = new JLabel("Pass");
+		passLabel.setBounds(10, 40, 80, 25);
+		panel.add(passLabel);
+
+		userText = new JTextField(20);
+		userText.setBounds(100, 10, 160, 25);
+		panel.add(userText);
+
+		passText = new JPasswordField(20);
+		passText.setBounds(100, 40, 160, 25);
+		panel.add(passText);
+
+		JButton button = new JButton("확인");
+
+		button.addActionListener(new ActionListener() 
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				dispose();
+			}
+		});
+
+		button.setBounds(60, 70, 80, 25);
+		panel.add(button);
+
+		JButton button2 = new JButton("취소");
+		button2.setBounds(140, 70, 80, 25);
+		
+		button2.addActionListener(new ActionListener() 
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				dispose();
+			}
+		});
+
+		panel.add(button2);
+
+	}
+
+	boolean login(String id, String pw) { 
+
+		
+			return true;
+	}
+
+}
---
+++ src/UI/Remote/PushView.java
@@ -0,0 +1,20 @@
+package UI.Remote;
+
+import java.awt.Container;
+
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.RootPaneContainer;
+
+public class PushView extends JDialog {
+	
+	public PushView(JPanel panel) 
+	{
+		setSize(280, 150);
+		setResizable(false);
+		setLocation(800, 450);
+		setVisible(true);
+	}
+	
+	
+}
